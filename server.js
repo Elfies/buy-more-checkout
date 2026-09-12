e8c0c1abaaad7c80a814b559ab847f75c5e7abac
Yasmina El Fassi <yasminaelfassi@Yasminas-MacBook-Air.local>
2026-09-12 13:04:05 +0100

    Add express server and placeholder storefront page

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
