9128ed356bb8b29ec76072770109b45d96c61da2
Yasmina El Fassi <elfassi.yasmina@gmail.com>
2026-09-12 14:25:15 +0100

    Add product catalogue, products endpoint and storefront

---
+++ products.js
@@ -0,0 +1,7 @@
+const products = [
+    { id: 'bm-001', name: 'Wireless Headphones', price: 14900 },
+    { id: 'bm-002', name: 'Bluetooth Speaker', price: 8900 },
+    { id: 'bm-003', name: 'USB-C Charging Cable', price: 2400 }
+  ];
+  
+  module.exports = products;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const products = require('./products');
 
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
